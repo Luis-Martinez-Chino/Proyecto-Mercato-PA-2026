027ad2254968eee86b259e4978854c9209d892d9
Luis-Martinez-Chino <luispmb22@gmail.com>
2026-09-06 15:13:05 -0600

    Mediocampista.h terminado

--- Mediocampista.h
+++ Mediocampista.h
@@ -2,3 +2,15 @@
 #include <iostream>
 #include <string>
 #include "Jugador.h"
+
+using namespace std;
+
+class Mediocampista : public Jugador {
+public:
+	Mediocampista(string nombre, int id, int valorMercado, string clubActual) : Jugador(nombre, id, valorMercado, clubActual) {
+	}
+	string mostrarDescripcion() override;
+	string getTipo() override;
+	~Mediocampista() override {
+	}
+};
