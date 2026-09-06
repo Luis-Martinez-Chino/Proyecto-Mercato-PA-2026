3ab55240284382e0bb93c2834652614d8cf4102f
Luis-Martinez-Chino <luispmb22@gmail.com>
2026-09-06 15:12:40 -0600

    Delantero.h terminado

--- Delantero.h
+++ Delantero.h
@@ -3,3 +3,14 @@
 #include <string>
 #include "Jugador.h"
 
+using namespace std;
+
+class Delantero : public Jugador {
+public:
+	Delantero(string nombre, int id, int valorMercado, string clubActual) : Jugador(nombre, id, valorMercado, clubActual) {
+	}
+	string mostrarDescripcion() override;
+	string getTipo() override;
+	~Delantero() override {
+	}
+};
