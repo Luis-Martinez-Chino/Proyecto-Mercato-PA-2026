61f0d2facbc7e46d0b565e149a5dae59a7e722e8
Luis-Martinez-Chino <luispmb22@gmail.com>
2026-09-06 15:12:04 -0600

    Defensa.h terminado
    Added Defensa class extending Jugador with overridden methods.

--- Defensa.h
+++ Defensa.h
@@ -2,3 +2,15 @@
 #include <iostream>
 #include <string>
 #include "Jugador.h"
+
+using namespace std;
+
+class Defensa : public Jugador {
+public:
+	Defensa(string nombre, int id, int valorMercado, string clubActual) : Jugador(nombre, id, valorMercado, clubActual) {
+	}
+	string mostrarDescripcion() override;
+	string getTipo() override;
+	~Defensa() override {
+	}
+};
