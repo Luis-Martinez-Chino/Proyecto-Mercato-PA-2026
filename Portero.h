431b6382ec13bd09cd40e89bfb56784c14bfd52f
Luis-Martinez-Chino <luispmb22@gmail.com>
2026-09-06 15:13:40 -0600

    Portero.h terminado

--- Portero.h
+++ Portero.h
@@ -6,11 +6,12 @@
 using namespace std;
 
 class Portero : public Jugador {
-	public:
-		Portero(string nombre, int id, int valorMercado, string clubActual) : Jugador(nombre, id, valorMercado, clubActual) {
-		}
-		string mostrarDescripcion() override;
-		string getTipo() override;
-		~ Portero() override {
-		}
+public:
+	Portero(string nombre, int id, int valorMercado, string clubActual) : Jugador(nombre, id, valorMercado, clubActual) {
+	}
+	string mostrarDescripcion() override;
+	string getTipo() override;
+	~Portero() override {
+	}
 };
+
